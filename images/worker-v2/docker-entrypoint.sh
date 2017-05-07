e39e0f41116e82cc735f35fd1a4651de46fe22df
alahiff <alahiff@users.noreply.github.com>
2017-05-07 16:47:37 +0100

    Update docker-entrypoint.sh

--- images/worker-v2/docker-entrypoint.sh
+++ images/worker-v2/docker-entrypoint.sh
@@ -3,6 +3,8 @@
 # Configuration
 echo "CONDOR_HOST = $CONDOR_HOST" > /etc/condor/config.d/docker
 echo "SEC_PASSWORD_FILE = $SEC_PASSWORD_FILE" >> /etc/condor/config.d/docker
+echo "NUM_CPUS = $CONDOR_CPUS" >> /etc/condor/config.d/docker
+echo "MEMORY = $CONDOR_MEMORY" >> /etc/condor/config.d/docker
 
 # Run HTCondor
 /usr/sbin/condor_master -f
